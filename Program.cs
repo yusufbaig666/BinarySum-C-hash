9a9d1907e2b85fb8f12945f760961b9b0cc26aa6
chandupavan <2000031152@kluniversity.in>
2023-08-30 22:07:00 +0530

    C#PROJECT

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    static void Main(string[] args)
+    {
+        int num1, num2, result;
+        num1 = Convert.ToInt32(Console.ReadLine());     
+        num2 = Convert.ToInt32(Console.ReadLine());
+        result = Add(num1, num2);
+        Console.WriteLine( result);
+        Console.ReadLine();
+    }
+}
